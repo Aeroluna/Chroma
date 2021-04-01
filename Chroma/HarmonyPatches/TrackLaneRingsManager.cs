a0ddd4313825f5a030c1bcddc01dd445699ebbad
Pixelguymm <73407947+Pixelguymm@users.noreply.github.com>
2021-04-01 12:47:36 +0200

    Add track lane ring modifiers

---
+++ Chroma/HarmonyPatches/TrackLaneRingsManager.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using CustomJSONData;
+using Chroma.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chroma.HarmonyPatches
+{
+    [HarmonyPatch(typeof(TrackLaneRingsManager))]
+    [HarmonyPatch("Awake")]
+    class TrackLaneRingsManagerAwake
+    {
+        private static void Prefix(ref int ____ringCount, ref float ____ringPositionStep)
+        {
+            dynamic ringData = StandardLevelScenesTransitionSetupDataSOInit._trackLaneRingModifiers;
+
+            if (ringData != null && ChromaConfig.Instance.EnvironmentEnhancementsEnabled)
+            {
+                if (Trees.at(ringData, "_ringCount") is long ringCount) ____ringCount = (int)ringCount;
+
+                if (Trees.at(ringData, "_ringPositionStep") is double ringPositionStepDbl) ____ringPositionStep = (float)ringPositionStepDbl;
+                else if (Trees.at(ringData, "_ringPositionStep") is long ringPositionStepLng) ____ringPositionStep = ringPositionStepLng;
+            }
+        }
+    }
+}
