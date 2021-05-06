fef57e53f9d7b2c526aed15c454030f4e75bb1f3
Caeden117 <caeden.s@outlook.com>
2021-05-05 20:38:49 -0700

    let 360/90 chevron be affected by alpha

---
+++ Chroma/HarmonyPatches/SpawnRotationChevron.cs
@@ -0,0 +1,19 @@
+﻿namespace Chroma.HarmonyPatches
+{
+    using UnityEngine;
+
+    [ChromaPatch(typeof(SpawnRotationChevron))]
+    [ChromaPatch("UpdateLights")]
+    internal static class SpawnRotationChevronSetLightAmount
+    {
+        private static bool Prefix(TubeBloomPrePassLight[] ____lights, Color ____color, float ____lightAmount)
+        {
+            foreach (TubeBloomPrePassLight light in ____lights)
+            {
+                light.color = ____color.ColorWithAlpha(____color.a * ____lightAmount);
+            }
+
+            return false;
+        }
+    }
+}
